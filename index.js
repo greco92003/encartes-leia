a3d35d33422c4caa19f9ab9d765e140c4b1b056e
Greco Marques <greco92003@gmail.com>
2025-04-25 23:33:46 -0300

    Corrige configuração para deploy no Vercel

---
+++ index.js
@@ -0,0 +1,3 @@
+// Este arquivo serve apenas para redirecionar para o diretório app
+// O Vercel usará o script vercel-build definido no package.json
+console.log('Redirecionando para o diretório app...');
